50971200a931919e830f66ab74ef7c40eb2e301a
Stan Bouchet <computer@Stans-MacBook-Pro.local>
2026-04-15 12:31:11 +0200

    Fix Astro 5 compatibility with @keystatic/astro and @astrojs/netlify

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,1 +1,48 @@
-export { k as createHandler } from './chunks/ssr-function_DxE9iJAz.mjs';
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_fmqatxaM.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/api/keystatic/_---params_.astro.mjs');
+const _page2 = () => import('./pages/blog.astro.mjs');
+const _page3 = () => import('./pages/blog/_---slug_.astro.mjs');
+const _page4 = () => import('./pages/engagement-associatif.astro.mjs');
+const _page5 = () => import('./pages/keystatic/_---params_.astro.mjs');
+const _page6 = () => import('./pages/parcours.astro.mjs');
+const _page7 = () => import('./pages/projets.astro.mjs');
+const _page8 = () => import('./pages/_slug_.astro.mjs');
+const _page9 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["node_modules/@keystatic/astro/internal/keystatic-api.js", _page1],
+    ["src/pages/blog/index.astro", _page2],
+    ["src/pages/blog/[...slug].astro", _page3],
+    ["src/pages/engagement-associatif.astro", _page4],
+    ["node_modules/@keystatic/astro/internal/keystatic-astro-page.astro", _page5],
+    ["src/pages/parcours.astro", _page6],
+    ["src/pages/projets.astro", _page7],
+    ["src/pages/[slug].astro", _page8],
+    ["src/pages/index.astro", _page9]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./noop-entrypoint.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "1cf45e8a-505c-4cb3-8eed-11ece5508c96"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (Object.prototype.hasOwnProperty.call(serverEntrypointModule, _start)) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
