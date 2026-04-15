9cf1656933efc98d38c0511df97b3f49a89de851
Stan Bouchet <computer@Stans-MacBook-Pro.local>
2026-04-15 15:55:42 +0200

    Enable SSR output and add graphql for Keystatic API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   site: 'https://cv-blog.netlify.app',
-  output: 'static',
+  output: 'server',
   adapter: netlify(),
   integrations: [
     react(),
